8ae3abee1b1f0e613dcad50ea83cac760acaa571
EquinoxIsNear <98333124+EquinoxIsNear@users.noreply.github.com>
2022-01-24 23:31:09 +0800

    added initial files
    added index.html, surveyStyle.css, index.js, index.jpg

---
+++ index.js
@@ -0,0 +1,8 @@
+let loading = document.getElementById("loading");
+
+setTimeout(function () {
+  loading.style.opacity = 0;
+  setTimeout(function () {
+    loading.style.display = "none";
+  }, 750);
+}, 250);
